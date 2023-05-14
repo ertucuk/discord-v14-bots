a24420701384eeb6f00e1d7dd778e23e8ff8e6cc
ertu <68440024+ertucuk@users.noreply.github.com>
2023-05-14 15:35:12 +0300

    Update snipe.js

--- src/commands/Staffs/snipe.js
+++ src/commands/Staffs/snipe.js
@@ -24,37 +24,7 @@ module.exports = {
       ],
     },
 
-    onLoad: function (client) {
-
-       client.on("messageDelete", async (message) => { 
-
-        await snipe.findOneAndUpdate({ guildID: message.guild.id, channelID: message.channel.id }, { $set: { messageContent: message.content, userID: message.author.id, image: message.attachments.first() ? message.attachments.first().proxyURL : null, createdDate: message.createdTimestamp, deletedDate: Date.now() } }, { upsert: true });
-        const channel = client.channels.cache.find(x => x.name == "message_log");
-        let entry = await message.guild.fetchAuditLogs({ type: AuditLogEvent.MessageDelete }).then(audit => audit.entries.first())
-        if (!channel) return;
-        const embed = new EmbedBuilder()
-          .setAuthor({ name : message.member.displayName, iconURL: message.author.avatarURL({ dynamic: true })})
-          .setDescription(`
-          ${entry.executor} tarafından ${message.channel} kanalında bir mesaj silindi !
-                              
-          \`•\` Mesaj Kanalı: ${message.channel} - (\`${message.channel.id}\`)
-          \`•\` Mesaj Sahibi: ${message.author} - (\`${message.author.id}\`)
-          \`•\` Mesaj İçeriği: \`\`\`${message.content.length > 300 ? "300 Karakterden uzun.." : `${message.content}`}\`\`\`
-          `)
-          .setFooter({ text: `ID: ${message.author.id} • ${moment(Date.now()).format("LLL")}`});
-          if (!message.attachments.first()) channel.send({ embeds: [embed]});
-      
-        const embedx = new EmbedBuilder()
-        .setAuthor({ name : message.member.displayName, iconURL: message.author.avatarURL({ dynamic: true })})
-        .setDescription(`
-        ${entry.executor} tarafından ${message.channel} kanalında bir içerik sildi!
-                            
-        \`•\` Mesaj Kanalı: ${message.channel} - (\`${message.channel.id}\`)
-        \`•\` Mesaj Sahibi: ${message.author} - (\`${message.author.id}\`)
-        `)
-        .setFooter({ text: `ID: ${message.author.id} • ${moment(Date.now()).format("LLL")}`});
-        if (message.attachments.first()) channel.send({ embeds: [embedx.setImage(message.attachments.first().proxyURL)]});
-})},
+    onLoad: function (client) { },
     
     onCommand: async function (client, message, args) {
 
