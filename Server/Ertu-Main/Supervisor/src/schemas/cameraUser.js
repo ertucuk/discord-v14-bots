947b6429f3d95a13af290c428c2fd746c7a91863
ertu <68440024+ertucuk@users.noreply.github.com>
2023-08-01 21:02:14 +0300

    Delete Server directory

--- Server/Ertu-Main/Supervisor/src/schemas/cameraUser.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const schema = Schema({
-  guildID: String,
-  userID: String,
-  topStat: { type: Number, default: 0 },
-});
-
-module.exports = model("cameraUser", schema);
--- Server/Ertu-Welcome/System.js
+++
@@ -1,5 +0,0 @@
-module.exports = {// buraya dokunmayiniz!!!!!!!!!!!!!!!!
-    mp3: true, 
-    file: "./ertu.mp3", 
-    youtubeURL: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
-  }
