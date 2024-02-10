db42c7a0b691f35ff4124a3e45f7dbaf292d1d60
ertu <68440024+ertucuk@users.noreply.github.com>
2024-02-10 22:07:33 +0300

    Update System.js

--- Global/Settings/System.js
+++ Global/Settings/System.js
@@ -1,10 +1,10 @@
 module.exports = {
-    Server: "Mentia", // sunucu adı
-    ServerID: "1144610315376537691", // sunucu id
-    ServerURL: "ertu", // sunucu urk
+    Server: "", // sunucu adı
+    ServerID: "", // sunucu id
+    ServerURL: "", // sunucu url
     SubTitle: "Ertu was here ❤️", // embed alt baslık
-    BotVoiceChannel: "1156991477424070656", // botların girecegis es
-    MongoURL: "mongodb+srv://ertununannesi:ertununannesi@cluster0.6oyu9nd.mongodb.net/ertu?retryWrites=true&w=majority", // mongodb
+    BotVoiceChannel: "", // botların girecegis es
+    MongoURL: "", // mongodb
     WebhookURL: "https://discord.com/api/webhooks/1152978895797244026/pXSA_TCtaerBMyaBuQVugzmvJ1DYzR3WJDcYOu7XfA_-Nf7DTDTvQa3PnnepZvlnQGTo", // webhook url kalabilir bnoyle
     BackGround: "https://cdn.discordapp.com/attachments/1121409922328956928/1121778487355772998/576388f3bdfea3f99ef4282c518f316b.png", // kalsın bu
     BotsOwners: ["136619876407050240"], // bot owner idleri
@@ -16,8 +16,8 @@ module.exports = {
     },
 
     Mainframe: {
-    Moderation: "MTA0NDkyNjY3OTcwNzAzMzYzMQ.G69cTu.E3CO6wDVZQnC_6_Bh5whTM1PKW_SsLyRyCc7fo", // moderation token
-    Registery: "ODM2NTU3MTU4MTQ1MTk2MDQz.G-9Mqp.ng4dGc2KaXNBMyg6YWfWD9ej-yWrJi1GLqEda8", // registration token
+    Moderation: "", // moderation token
+    Registery: "", // registration token
 
     EmojiNumbers: false,
     GlobalInteractions: true,
@@ -43,39 +43,22 @@ module.exports = {
     },
 
     Security: {
-        Guard_I: "ODI4NjQzNjAyMzc5OTY0NDg3.G-FTZT.3YZmfINAxDM74YjXxcBvVSL7qgWOLLog8a5-2Q",
-        Guard_II:  "ODI4Njk3Mzk5MDgxMzA0MDY0.Gad6_X.UbYTP6jccUugkvxH7_8Qu_c8CGsesCmBNvIBps",
-        Guard_III: "ODI5NzQ0ODIyNTkxODE1NzAx.GUeBVM.eNfOdpW1REJT9SI-BdE1tIwUFw1UJtah6OwRpM",
-        SelfBotToken: "321321321312312312312312", // url koruyan kullanıcı tokewni
-        UrlSpammer: "1146781809615573082", // guard-log kanal id
+        Guard_I: "",
+        Guard_II:  "",
+        Guard_III: "",
+        SelfBotToken: "", // url koruyan kullanıcı tokewni
+        UrlSpammer: "", // guard-log kanal id
         KufurEngel: true,
         CapsEngel: true,
         ReklamEngel: true,
         SpamEngel: true,
 
-        Dis: [
-            "ODM2NzA0OTIzOTc4NDMyNTIy.GVxvWq.ynDQBs0os5nvasSQw3bpw3oMVVV2ESVR1LsRSU", // dağıtıcılar
-            "ODM3ODMyNjUyMjA3NDIzNDk5.GgawRR.gkpWlcdaxeC7YD8XoEvOE_vzXR1cYKFS7eeriM"
-        ],
+        Dis: [],
     },
 
     Welcome: {
-        Tokens: [ 
-            "MTA4OTUxNTM4NTEyNjEzNzg1Ng.G_4SWd.zDUC8Ic2yxVQEj1VLkwyP0_0yeZ7yEEhY921qI", // welcomer tokenler
-            "MTA5MDA2NjY1MjU0MjAwOTM0NA.G-YrUY.uDzSRK95eJBsaUQQZtZD1Mlxopp03D_0UfOPS8",
-            "MTA5MDA2NzE2Nzk5MjYxOTA5OA.GwVSTl.o3cfWwh44asHdZKAJusQAVP0rxCUSuTdxNjss8",
-            "MTA5MDA2NzUwNzc4NTc2OTA0MQ.Gebzkq.npxro5ek-Papd7-BhSm5WGV07FGusbob_ys81Q"
-      
-          ],
-
-        Channels: [ 
-          "1103775446065225758", 
-          "1103775449374539826", 
-          "1103775453447200909", 
-          "1103775456504860712"
-      
-          ],
-
-          Staff: "",
+        Tokens: [],
+        Channels: [],
+        Staff: "",
     }
 } 
